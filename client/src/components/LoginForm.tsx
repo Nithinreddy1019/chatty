2ca99cef20beb07c64c85789d9588f8b156e45e0
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-31 23:48:14 +0530

    ui: Added login form'

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "./ui/Footer";
 import { Header } from "./ui/Header";
+import { InputBox } from "./ui/InputBox";
 
 
 export const Loginform = () => {
@@ -11,18 +12,40 @@ export const Loginform = () => {
 
             <div
                 className="p-2 rounded-xl shadow-lg border-2 border-blue-500
-                flex flex-col items-center"
+                flex flex-col items-center
+                min-w-[400px]"
             >
                 <Header 
                     headerLabel="Welcome back"
                     headerSubLabel="Login to continue"
                 />
 
+                <InputBox 
+                    inputLabel="Email"
+                    inputPlaceholder="JohnDoe@gmail.com"
+                    inputType="email"
+                />
+
+                <InputBox 
+                    inputLabel="Password"
+                    inputPlaceholder="******"
+                    inputType="password"
+                />
+
+                <button
+                    className="px-2 py-1 my-2 rounded-lg w-fit
+                    bg-blue-500 text-white font-bold hover:scale-[102%]"
+                >
+                    Login
+                </button>
+                
+
                 <Footer 
                     backButtonLabel="Don't have an account?"
                     backButtonLinkLabel="Sign up"
                     backButtonHref="/register"
                 />
+
             </div>
 
         </main>
