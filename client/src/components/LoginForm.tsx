1dd8b744861b54b00eb024805426ecf5a27af44d
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-01 15:29:57 +0530

    ui: changed login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -2,6 +2,9 @@ import { Footer } from "./ui/Footer";
 import { Header } from "./ui/Header";
 import { InputBox } from "./ui/InputBox";
 
+import authsvg from "../assets/authsvg.svg";
+
+
 
 export const Loginform = () => {
     return (
@@ -10,41 +13,51 @@ export const Loginform = () => {
             bg-blue-100"
         >
 
-            <div
-                className="p-2 rounded-xl shadow-lg border-2 border-blue-500
-                flex flex-col items-center
-                min-w-[400px]"
-            >
-                <Header 
-                    headerLabel="Welcome back"
-                    headerSubLabel="Login to continue"
-                />
+            <div className="flex items-center
+            p-2 rounded-xl shadow-lg border-2 border-blue-500">
 
-                <InputBox 
-                    inputLabel="Email"
-                    inputPlaceholder="JohnDoe@gmail.com"
-                    inputType="email"
-                />
+                <div>
+                    <img src={authsvg} className="w-96 h-96"/>
+                </div>
 
-                <InputBox 
-                    inputLabel="Password"
-                    inputPlaceholder="******"
-                    inputType="password"
-                />
-
-                <button
-                    className="px-2 py-1 my-2 rounded-lg w-fit
-                    bg-blue-500 text-white font-bold hover:scale-[102%]"
+                <div
+                    className="p-2 
+                    flex flex-col items-center
+                    min-w-[400px]"
                 >
-                    Login
-                </button>
-                
+                    <Header 
+                        headerLabel="Welcome to Chatty"
+                        headerSubLabel="Login to continue"
+                    />
 
-                <Footer 
-                    backButtonLabel="Don't have an account?"
-                    backButtonLinkLabel="Sign up"
-                    backButtonHref="/register"
-                />
+                    <InputBox 
+                        inputLabel="Email"
+                        inputPlaceholder="JohnDoe@gmail.com"
+                        inputType="email"
+                    />
+
+
+                    <InputBox 
+                        inputLabel="Password"
+                        inputPlaceholder="******"
+                        inputType="password"
+                    />
+
+                    <button
+                        className="px-2 py-1 my-2 rounded-lg w-fit
+                        bg-blue-500 text-white font-bold hover:scale-[102%]"
+                    >
+                        Login
+                    </button>
+                    
+
+                    <Footer 
+                        backButtonLabel="Don't have an account?"
+                        backButtonLinkLabel="Sign up"
+                        backButtonHref="/register"
+                    />
+
+                </div>
 
             </div>
 
