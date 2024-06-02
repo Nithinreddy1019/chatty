4c14f110597db532120c4b8bc015b611d639d7c6
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-02 21:49:03 +0530

    db: mongo, prisma

---
+++ server/dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const user_auth_1 = require("./routes/user-auth");
+const port = 3000;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: "http://localhost:5173"
+}));
+app.use((0, cookie_parser_1.default)());
+dotenv_1.default.config();
+console.log(process.env.DATABASE_URL);
+app.use("/app/v1", user_auth_1.userRouter);
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.json({ msg: "test OK" });
+}));
+app.listen(3000, () => {
+    console.log(`Listening on PORT - ${port}`);
+});
