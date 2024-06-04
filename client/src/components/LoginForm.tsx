ff7b5e42eebfe1a508f635cebc8f06f220214f80
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-06-04 12:18:05 +0530

    form added

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -3,10 +3,43 @@ import { Header } from "./ui/Header";
 import { InputBox } from "./ui/InputBox";
 
 import authsvg from "../assets/authsvg.svg";
+import { useTransition } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { LoginSchema } from "../schema/auth-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
+
+interface IFormInput {
+    email: string,
+    password: string
+}
 
 
 export const Loginform = () => {
+
+    const [isPending, startTransition] = useTransition();
+
+    const {
+        register,
+        handleSubmit, 
+        setError,
+        formState: { errors }
+    }= useForm<IFormInput>({
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+        resolver: zodResolver(LoginSchema)
+    });
+
+
+    const onSubmit: SubmitHandler<IFormInput> = (values) => {
+        console.log("sumitted")
+        console.log(values);
+    };
+
+
     return (
         <main
             className="h-screen flex items-center justify-center
@@ -22,20 +55,35 @@ export const Loginform = () => {
                     <img src={authsvg} className="w-96 h-96"/>
                 </div>
 
-                <div
+                <form
                     className="p-2 
                     flex flex-col items-center
                     w-[350px]"
+                    onSubmit={handleSubmit(onSubmit)}
                 >
                     <Header 
                         headerLabel="Welcome to Chatty"
                         headerSubLabel="Login to continue"
                     />
 
-                    <InputBox 
+                    <input 
+                        placeholder="email"
+                        {...register("email")}
+                    />
+                    {errors && <p>{errors.email?.message}</p>}
+
+                    <input 
+                        placeholder="password"
+                        {...register("password")}
+                    />
+                    {errors && <p>{errors.password?.message}</p>}
+
+                    {/* <InputBox 
                         inputLabel="Email"
                         inputPlaceholder="JohnDoe@gmail.com"
                         inputType="email"
+                        register={register}
+                        inputProperty="email"
                     />
 
 
@@ -43,11 +91,14 @@ export const Loginform = () => {
                         inputLabel="Password"
                         inputPlaceholder="******"
                         inputType="password"
-                    />
+                        register={register}
+                        inputProperty="password"
+                    /> */}
 
                     <button
                         className="px-2 py-1 my-2 rounded-lg w-fit
                         bg-blue-500 text-white font-bold hover:scale-[102%]"
+                        type="submit"
                     >
                         Login
                     </button>
@@ -59,7 +110,7 @@ export const Loginform = () => {
                         backButtonHref="/register"
                     />
 
-                </div>
+                </form>
 
             </div>
 
